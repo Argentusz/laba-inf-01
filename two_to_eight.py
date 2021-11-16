a1e1dfa0a5f3baaf14a24178edd4fe713f9b1657
mishhgun01 <67867944+mishhgun01@users.noreply.github.com>
2021-11-16 20:49:23 +0300

    Update two_to_eight.py

--- two_to_eight.py
+++ two_to_eight.py
@@ -1,4 +1,5 @@
 def two_to_eight(s)->str:
     if len(s) == 0:
-        return ''
+        return ''+"1"
     return two_to_eight(s[:-3]) + str(int(s[-3:], 2))
+#returns 8-number with "1" at the first position
