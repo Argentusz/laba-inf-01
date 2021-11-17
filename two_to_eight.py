26a20b7b9826a4e73f738e4e787b0d45c2bb668c
mishhgun01 <67867944+mishhgun01@users.noreply.github.com>
2021-11-17 20:43:30 +0300

    Add files via upload

--- two_to_eight.py
+++ two_to_eight.py
@@ -1,5 +1,9 @@
-def two_to_eight(s)->str:
+def two_to_eight(s) -> str:
     if len(s) == 0:
         return ''+"1"
-    return two_to_eight(s[:-3]) + str(int(s[-3:], 2))
-#returns 8-number with "1" at the first position
+    st = two_to_eight(s[:-3]) + str(int(s[-3:], 2))
+    # k = int(st)+1
+    # st=str(k)
+    return st
+
+print(two_to_eight("1010110010110"))
