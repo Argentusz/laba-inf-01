b5c865735d00be1229ddf99dc702711866f44a89
mishhgun01 <67867944+mishhgun01@users.noreply.github.com>
2021-11-19 13:19:18 +0300

    Delete two_to_eight.py

--- two_to_eight.py
+++
@@ -1,9 +0,0 @@
-def two_to_eight(s) -> str:
-    if len(s) == 0:
-        return ''+"1"
-    st = two_to_eight(s[:-3]) + str(int(s[-3:], 2))
-    # k = int(st)+1
-    # st=str(k)
-    return st
-
-print(two_to_eight("1010110010110"))
