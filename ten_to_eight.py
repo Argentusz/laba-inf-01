0d06589ae381e92ed3c772d296d592041ad7e4a9
mishhgun01 <67867944+mishhgun01@users.noreply.github.com>
2021-11-19 13:27:43 +0300

    Update ten_to_eight.py

--- ten_to_eight.py
+++ ten_to_eight.py
@@ -1,11 +1,16 @@
-def ten_to_eight(n)->str:
-    n = str(n)
-    n = int(n[::-1])
-    s =str(oct(n))
-    ar = list(map(str, list(s)))
-    del ar[0]
-    del ar[0]
-    s=''
-    for i in ar:
-       s+=i
-    return s
+def ten_to_eight(n)->(str, str):
+    try:
+        n = str(n)
+        n = int(n[::-1])
+        s =str(oct(n))
+        ar = list(map(str, list(s)))
+        del ar[0]
+        del ar[0]
+        s=''
+        for i in ar:
+           s+=i
+    except TypeError:
+        return '', 'u10-8_err'
+    if(s[0]!="1"):
+        return '', 'u10-8_err_missing_1_point_left'
+    return s,0
