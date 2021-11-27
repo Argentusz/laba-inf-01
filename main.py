c67874c761dddeb58d0aa07356cbdc980bc8506c
Argentusz <92330982+Argentusz@users.noreply.github.com>
2021-11-27 14:25:43 +0300

    Added Generator of Random Passwords

--- main.py
+++ main.py
@@ -22,15 +22,24 @@ SLEEP_FOR_OP = {1: 0.11, 2: 0.11, 3: 0.11, 4: 0.11, 5: 0.2, 6: 0.4, 7: 0.12, 8: 
                 44: 0.1, 45: 0.1, 46: 0.1, 47: 0.1, 48: 0.1, 49: 0.1, 50: 0.05, 51: 0.05, 52: 0.05, 53: 0.05, 54: 0.05,
                 55: 0.05, 56: 0.05, 57: 0.05, 58: 0.05, 59: 0.05, 60: 0.05, 61: 0.05, 62: 0.05, 63: 0.05, 64: 0.05,
                 65: 0.05, 66: 0.05, 67: 0.05, 68: 0.05, 69: 0.11}
-# Temporary password & code for debugging/developing
-TEMP_PASSWORD = 'password'
-TEMP_CODE = comp(TEMP_PASSWORD)
-print(TEMP_CODE)
+
+
+# Generating random password with difficulty
+def get_password(seed, dif):
+    if dif == 0:
+        password_pick = {0: 'abc', 1: '111', 2: 'qw'}
+    elif dif == 1:
+        password_pick = {0: 'password', 1: '1984', 2: 'qwerty'}
+    elif dif == 2:
+        password_pick = {0: 'yjemyp3y3xz8syev', 1: '6ck5s4xqf3nks6qa', 2: '95shzpmjfk2vdtg8', 3: 'mfyujrvqdtj3w8sh'}
+    else:
+        password_pick = {0: 'password'}
+    return password_pick[seed % len(password_pick)]
 
 
 # Checking if password is correct
-def fact_check(text):
-    return TEMP_PASSWORD == text
+def fact_check(text, password):
+    return password == text
 
 
 # Player failed in hacking
@@ -87,6 +96,9 @@ def draw_opening(i):
 
 # Program Entry point
 def main():
+    password = ''  # If nothing will go wrong this will be changed
+    seed = int(time.time())  # Seed For random numbers
+    print(seed)
     life = 3  # Attempts user have to hack
     clock = pygame.time.Clock()  # Used for limiting FPS
     run = True  # Game is running while this is set to True
@@ -117,12 +129,15 @@ def main():
                     elif event.key == pygame.K_RETURN:
                         if j % 3 == 1:  # Start the game
                             mode = 2
+                            dif = 1  # The difficulty should be picked by user. 2b fixed
+                            password = get_password(seed, dif)  # Generate password with this diff
+                            code = comp(password)  # Get password coded
                         if j % 3 == 0:  # Quit
                             run = False
                 # -- Gameplay -- #
                 if mode == 3:
                     if event.key == pygame.K_RETURN:  # Checking password when Enter is pressed
-                        if fact_check(text):
+                        if fact_check(text, password):
                             mode = 4  # Go to success screen
                         else:
                             life -= 1  # -1 Attempt
